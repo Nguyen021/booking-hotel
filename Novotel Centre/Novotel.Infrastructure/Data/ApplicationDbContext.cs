c72be56ed16623aee6267881fa2f5820c21b094d
Nguyen Duong <duongnguyen02102001@gmail.com>
2023-11-22 20:21:00 +0700

    add migrate house and seed data

--- Novotel Centre/Novotel.Infrastructure/Data/ApplicationDbContext.cs
+++ Novotel Centre/Novotel.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Novotel.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,46 @@ using System.Threading.Tasks;
 
 namespace Novotel.Infrastructure.Data
 {
-    public class ApplicationDbContext : DbContext 
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<House> Houses { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<House>().HasData(
+                new House
+                {
+                    Id = 1,
+                    Name = "Royal House",
+                    Description = "",
+                    ImageUrl= "https://placehold.co/600x400",
+                    Occupancy=4,
+                    Price=400,
+                    Sqft=550,
+                },
+                new House
+                {
+                    Id = 2,
+                    Name = "Royal House",
+                    Description = "",
+                    ImageUrl = "https://placehold.co/600x400",
+                    Occupancy = 3,
+                    Price = 300,
+                    Sqft = 540,
+                },
+                new House
+                {
+                    Id = 3,
+                    Name = "Royal House",
+                    Description = "",
+                    ImageUrl = "https://placehold.co/600x400",
+                    Occupancy = 4,
+                    Price = 500,
+                    Sqft = 650,
+                }
+                );
+        }
     }
+   
 }
---
+++ Novotel Centre/Novotel.Infrastructure/Migrations/20231122131834_addSeedDataToHouse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Novotel.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSeedDataToHouse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Houses",
+                columns: new[] { "Id", "CreatedDate", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2023, 11, 22, 20, 18, 34, 28, DateTimeKind.Local).AddTicks(6752), "", "https://placehold.co/600x400", "Royal House", 4, 400.0, 550, null },
+                    { 2, new DateTime(2023, 11, 22, 20, 18, 34, 28, DateTimeKind.Local).AddTicks(6767), "", "https://placehold.co/600x400", "Royal House", 3, 300.0, 540, null },
+                    { 3, new DateTime(2023, 11, 22, 20, 18, 34, 28, DateTimeKind.Local).AddTicks(6769), "", "https://placehold.co/600x400", "Royal House", 4, 500.0, 650, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Houses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Houses",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Houses",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
