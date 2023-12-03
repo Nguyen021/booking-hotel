508c3d8d1819c1b5783e0b3e234ca10d16dfa009
Nguyen Duong <duongnguyen02102001@gmail.com>
2023-12-03 20:36:36 +0700

    fix error tracking when update with view model

--- Novotel Centre/Novotel/Controllers/HouseNumberController.cs
+++ Novotel Centre/Novotel/Controllers/HouseNumberController.cs
@@ -28,36 +28,99 @@ namespace Novotel.Controllers
 
             //ViewData["Houses"] = houses;
 
-            IEnumerable<SelectListItem> list = _context.Houses.ToList().Select(h => new SelectListItem
-            {
-                Text = h.Name,
-                Value = h.Id.ToString()
-            });
+            //IEnumerable<SelectListItem> list = _context.Houses.ToList().Select(h => new SelectListItem
+            //{
+            //    Text = h.Name,
+            //    Value = h.Id.ToString()
+            //});
 
-            ViewData["HouseList"] = list;
-            return View();
+            //ViewBag.List = list;
+
+
+            HouseNumberVM houseNumberVM = new HouseNumberVM  {
+                HouseList = _context.Houses.ToList().Select(h => new SelectListItem
+                {
+                    Text = h.Name,
+                    Value = h.Id.ToString()
+                })
+            };
+            return View(houseNumberVM);
 
 
         }
         [HttpPost]
-        public IActionResult Create(HouseNumber houseNumber) 
+        public IActionResult Create(HouseNumberVM obj) 
         {
             //ModelState.Remove("House");
-            if(ModelState.IsValid)
+
+            bool checkExists = _context.HousesNumbers.Any(h=>h.House_Number.Equals(obj.HouseNumber.House_Number));
+            
+           
+            if (ModelState.IsValid && !checkExists)
             {
-                _context.HousesNumbers.Add(houseNumber);
-                _context.SaveChanges();
-                TempData["success"] = "The house number has been created successfully.";
-                return RedirectToAction("Index", "HouseNumber");
+                try
+                {
+                    _context.HousesNumbers.Add(obj.HouseNumber);
+                    _context.SaveChanges();
+                    TempData["success"] = "The house number has been created successfully.";
+                    return RedirectToAction("Index", "HouseNumber");
+                }catch(Exception ex) {
+                    TempData["error"] = "error somthing";
+                    return View();
+                }
             }
-
-            //TempData["error"] = "Something error try again!";
-            //var houses = _context.Houses.ToList();
-            //ViewData["Houses"] = houses;
-
-            return View();
+            if (checkExists)
+            {
+                TempData["error"] = "The house number already exists!";
+                
+            }
+            obj.HouseList = _context.Houses.ToList().Select(h => new SelectListItem
+            {
+                Text = h.Name,
+                Value = h.Id.ToString()
+            });
+            return View(obj);
         }
 
-        
+        public IActionResult Update(int houseNumberId) 
+        {
+            
+            HouseNumberVM houseNumberVM = new()
+            {
+                HouseList = _context.Houses.ToList().Select(h => new SelectListItem
+                {
+                    Text = h.Name,
+                    Value = h.Id.ToString()
+                }),
+                HouseNumber = _context.HousesNumbers.FirstOrDefault(x => x.House_Number.Equals(houseNumberId))
+            };
+            if (houseNumberVM == null) {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(houseNumberVM);
+        }
+        [HttpPost]
+        public IActionResult Update(HouseNumberVM obj)
+        {
+          
+            if (ModelState.IsValid )
+            {
+               
+                    _context.HousesNumbers.Update(obj.HouseNumber);
+                    _context.SaveChanges();
+                    TempData["success"] = "The house number has been updated successfully.";
+                    return RedirectToAction("Index", "HouseNumber");
+              
+            }
+
+            obj.HouseList = _context.Houses.ToList().Select(h => new SelectListItem
+                {
+                    Text = h.Name,
+                    Value = h.Id.ToString()
+            });
+            
+            return View(obj);
+
+        }
     }
 }
